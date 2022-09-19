678c1d6e65322979bbf51818224096abe9a7d980
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2022-09-19 23:53:13 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import HeaderComponent from './components/header'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
     <>
-      <HeaderComponent name='asd'/>
-      <HeaderComponent name='asxd'/>
-      <HeaderComponent name='assd'/>
-      <HeaderComponent name='123'/>
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
