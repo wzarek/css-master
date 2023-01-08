a2954a43895026fa618e0b434b192c396d49f8d9
domi802 <64065747+domi802@users.noreply.github.com>
2023-01-08 23:10:07 +0100

    Changing design of property menu

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropertiesMenu from './PropertiesMenu'
 import WelcomeInfo from './WelcomeInfo'
 import Menu from './menu/Menu'
+import Property from './Property'
 const Homepage = () => {
   return (
     <>
--- src/components/homepage/PropertiesMenu.js
+++ src/components/homepage/PropertiesMenu.js
@@ -3,7 +3,7 @@ import Property from './Property'
 
 const PropertiesMenu = () => {
   return (
-    <div className='flex flex-row justify-evenly mt-4'>
+    <div className='flex flex-row flex-wrap gap-1 justify-center mt-4'>
       <Property name='Border-radius' url='/temp' />
       <Property name='Animation' />
       <Property name='Box-shadow' />
